0f941e25b2e4f0a5d6bf9e495878ecebea687368
dev3Nigerian <faithfulodigbo@gmail.com>
2024-11-16 00:13:52 +0100

    updated next cofig file

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// // // export default nextConfig;
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   // Your config options here
+// //   reactStrictMode: true,
+// //   swcMinify: true,
+// // }
+
+// module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
